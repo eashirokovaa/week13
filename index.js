568d62dadd8237403f40b675266e505f03a20247
eashirokovaa <eashirokovaa@gmail.com>
2022-10-06 16:14:41 +0300

    2 tasks in one file

---
+++ index.js
@@ -0,0 +1,30 @@
+function GetName()
+{
+let name = prompt('Ваше имя?');
+alert(`Привет, ${name}`);
+}
+function calcSum()
+{
+    let a = Number(prompt('Первое число?')); 
+    let b = Number(prompt('Второе число?'));
+    alert(a+b);
+}
+function calcSubtract()
+{
+    let a = Number(prompt('Первое число?')); 
+    let b = Number(prompt('Второе число?'));
+    alert(a-b);
+}
+function calcMult()
+{
+    let a = Number(prompt('Первое число?')); 
+    let b = Number(prompt('Второе число?'));
+    alert(a*b);
+}
+function calcDiv()
+{
+    let a = Number(prompt('Первое число?')); 
+    let b = Number(prompt('Второе число?'));
+    alert(a/b);
+}
+alert(" \t \n" - 2 )
